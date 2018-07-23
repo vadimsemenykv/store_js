92e92f7fc39756e771a22f8ac973d6a308d3c90b
Vadym Semeniuk <vadimsemenykv@gmail.com>
2018-07-23 21:31:18 +0300

    26

--- src/front/components/account/OfferItem.js
+++ src/front/components/account/OfferItem.js
@@ -55,6 +55,7 @@ export default class OfferItem extends React.Component {
     }
 
     declineAndPropose() {
+        alert('In the deployment process');
         console.log('declineAndPropose');
     }
 
